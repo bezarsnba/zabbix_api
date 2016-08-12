3b676034cf3c241f967458065d1cd032fb2c2c4c
bezarsnba <bezarsnba@gmail.com>
2016-08-12 14:16:04 -0300

    Update update_action.py

--- update_action.py
+++ update_action.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Script para desabilitar ações
+# Script to disabled action
 # statusAction= 1(Disabled) 0 (enabled)
 # Autor: Bezaleel Ramos da Silva
 #
 
 from zabbixAPI import zapi
 from sys import argv
-auth = zapi( '', 'user.login', '' )
+
 
 statusAction=argv[1]
 
