2c8695a8f7da2f597eeb84555623f875ceace90c
bezarsnba <bezarsnba@gmail.com>
2017-02-08 10:39:39 -0300

    Create add_item_trigger.py
    Simple script to create item/trigger in lote at Zabbix using pyzabbix

---
+++ add_item_trigger.py
@@ -0,0 +1,48 @@
+
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+### Simple script to create item/trigger in lote at Zabbix
+### Parameter in file criaritem.txt:
+###
+### Ex: script.sh[Function,{HOST.IP},{HOST.NAME,000]
+
+#URL
+ZABBIX_SERVER = 'http://localhost/zabbix/'
+zapi = ZabbixAPI(ZABBIX_SERVER)
+
+#AUTH
+zapi.login('Admin','zabbix')
+
+ArqHost = open("C:\criaritem.txt", 'r').read().split('\n')
+
+def criaItem():
+    for linha in ArqHost:
+        #zItemHost = linha.split('')
+        zSistemaItem = linha.split(',')
+       
+        if linha:
+            item=zapi.item.create(
+            hostid = "10000",
+            name = "Name item %s" % zSistemaItem[2],
+            key_= linha,
+            type = 10,
+            interfaceid = 615,
+            delay =600,
+            value_type = 2,
+            history = 7,
+            status=0,
+            )
+
+        continue
+
+def criaTrigger():
+    for linha in ArqHost:
+        zItemHost = linha.split(',')
+        ZCreateTrigger= zapi.trigger.create(
+        host='10000',
+        description="Name Item %s " % ( zItemHost[2]), 
+        status=0,
+        type=0,
+        priority=2,
+            expression='{Zabbix server:script.sh[Function,{HOST.IP},{HOST.NAME},000].str("ERROR")}=1' % zItemHost[2]
+            )
+
